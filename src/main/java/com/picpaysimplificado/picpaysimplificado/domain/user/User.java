2bb10a89eee6c3997604ca08f9ad7632e32c3026
jefersonprimer <jefersonpriimer@gmail.com>
2024-12-03 08:20:00 -0300

    feat(UserType class): for a variable UserType of User

--- src/main/java/com/picpaysimplificado/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/domain/user/User.java
@@ -6,6 +6,8 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Entity(name="users")
 @Table(name="users")
 @Getter
@@ -14,6 +16,16 @@ import lombok.Setter;
 @EqualsAndHashCode(of="id")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY);
-    private long id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String document;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private BigDecimal balance;
+    private UserType userType;
+
 }
---
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/domain/user/UserType.java
@@ -0,0 +1,4 @@
+package com.picpaysimplificado.picpaysimplificado.domain.user;
+
+public class UserType {
+}
