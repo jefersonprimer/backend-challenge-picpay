ec910535b627ad5c61099b95aa970a1ed18e46e8
jefersonprimer <jefersonpriimer@gmail.com>
2024-12-04 07:45:30 -0300

    feat: endpoints

--- src/main/java/com/picpaysimplificado/picpaysimplificado/services/UserService.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/services/UserService.java
@@ -2,11 +2,13 @@ package com.picpaysimplificado.picpaysimplificado.services;
 
 import com.picpaysimplificado.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.picpaysimplificado.dtos.UserDTO;
 import com.picpaysimplificado.picpaysimplificado.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -25,6 +27,16 @@ public class UserService {
 
     public User findUserById(Long id) throws Exception {
        return this.repository.findUserById(id).orElseThrow(() -> new Exception("usuário não encontrado"));
+    }
+
+    public User createUser(UserDTO data) {
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers() {
+        return this.repository.findAll();
     }
 
     public void saveUser(User user) {
