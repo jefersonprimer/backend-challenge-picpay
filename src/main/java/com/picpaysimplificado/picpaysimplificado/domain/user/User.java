f6c6373073ec6812629b2f5bba08add2afedb083
jefersonprimer <jefersonpriimer@gmail.com>
2024-12-04 08:24:57 -0300

    feat: create service ,but no exception tratament

--- src/main/java/com/picpaysimplificado/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/domain/user/User.java
@@ -2,10 +2,7 @@ package com.picpaysimplificado.picpaysimplificado.domain.user;
 
 import com.picpaysimplificado.picpaysimplificado.dtos.UserDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -14,6 +11,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of="id")
 public class User {
     @Id
@@ -36,6 +34,7 @@ public class User {
         this.balance = data.balance();
         this.userType = data.userType();
         this.password = data.password();
+        this.document = data.document();
         this.email = data.email();
     }
 }
--- src/main/java/com/picpaysimplificado/picpaysimplificado/services/NotificationService.java
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/services/NotificationService.java
@@ -16,12 +16,14 @@ public class NotificationService {
     public void sendNotification(User user, String message) throws Exception {
         String email = user.getEmail();
         NotificationDTO notificationRequest = new NotificationDTO(email, message);
-
+        /*
         ResponseEntity<String> notificationResponse = restTemplate.postForEntity("http://o4d9z.mocklab.io/notify", notificationRequest, String.class);
 
         if(!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
             System.out.println("Error ao enviar notificação");
             throw new Exception("Serviço de notificação esta fora do ar");
         }
+        */
+        System.out.println("Notificação enviada para o usuario");
     }
 }
